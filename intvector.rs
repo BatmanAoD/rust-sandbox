f35113fc95824c81aa1caa5607966f3c08dcbb8f
Kyle Strand <kyle.strand@beckman.com>
2015-05-04 19:44:08 -0600

    Starting a simple "Linked list" implementation

---
+++ intvector.rs
@@ -0,0 +1,28 @@
+// Data
+
+struct Node
+{
+    data_seg_: [i32; 64],
+    next_: Option<Box<Node>>,
+    size_: i32,
+}
+
+pub struct IntVector
+{
+    head_: Box<Node>,
+}
+
+// Requests
+
+impl std::ops::IndexMut<i32> for Node
+{
+    fn index_mut(&'a mut self, index: i32) -> &'a mut i32
+    {
+        self.data_seg_[index]
+    }
+}
+
+impl Node
+{
+}
+
